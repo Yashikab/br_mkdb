fd63bd5feb752d290a59fef7edf31d18d7d81deb
Yashikab <yashikaba@gmail.com>
2020-04-15 00:36:24 +0900

    update parse info and refactor

--- src/module/getdata.py
+++ src/module/getdata.py
@@ -18,7 +18,7 @@ class OfficialProgram:
             'div.table1.is-tableFixed__3rdadd > table'
         self.__target_table_html = __soup.select_one(__target_table_selector)
 
-    def getname(self, row: int) -> str:
+    def getplayerinfo2dict(self, row: int) -> dict:
         # 各選手の行を選択 -> list
         player_info_html_list = self.__target_table_html.select('tbody')
         assert len(player_info_html_list) == 6, \
@@ -36,8 +36,19 @@ class OfficialProgram:
         player_no_level, name, birthplace_age_weight = \
             list(map(lambda elements: elements.text, player_name_list))
         name = name.replace('\n', '').replace('\u3000', '')
+        player_id, player_level = player_no_level.replace('\r', '')\
+                                                 .replace('\n', '')\
+                                                 .replace(' ', '')\
+                                                 .split('/')
+        player_id = int(player_id)
 
-        return name
+        content_dict = {
+            'name': name,
+            'id': player_id,
+            'level': player_level
+        }
+
+        return content_dict
 
 
 class GetDataTest(unittest.TestCase):
@@ -46,18 +57,34 @@ class GetDataTest(unittest.TestCase):
     番組表
     '''
     # 選手情報-氏名の取得
-    def test_getplayer_name(self):
+    def test_getplayer_info(self):
         self.sample_url = 'http://boatrace.jp/owpc/pc/race/racelist?'\
                           'rno=3&jcd=06&hd=20200408'
         op = OfficialProgram(self.sample_url)
         # 1列目
-        with self.subTest(row=1):
-            name = op.getname(row=1)
+        player_info = op.getplayerinfo2dict(row=1)
+        # name
+        with self.subTest(info='row1 name'):
+            name = player_info['name']
             self.assertEqual("鈴木裕隆", name)
+        with self.subTest(info='row1 id'):
+            player_id = player_info['id']
+            self.assertEqual(4231, player_id)
+        with self.subTest(info='row1 level'):
+            player_id = player_info['level']
+            self.assertEqual('B1', player_id)
+
         # 2列目
-        with self.subTest(row=2):
-            name = op.getname(row=2)
+        player_info = op.getplayerinfo2dict(row=2)
+        with self.subTest(info='row2 name'):
+            name = player_info['name']
             self.assertEqual("小林晋", name)
+        with self.subTest(info='row2 id'):
+            player_id = player_info['id']
+            self.assertEqual(4026, player_id)
+        with self.subTest(info='row2 level'):
+            player_id = player_info['level']
+            self.assertEqual('B1', player_id)
 
 
 if __name__ == '__main__':
