41483e048b9209baf66247def151fbd76c1bacdb
Yashikab <yashikaba@gmail.com>
2020-07-22 08:46:09 +0900

    fxxx

--- src/which_db.py
+++ src/which_db.py
@@ -6,14 +6,42 @@ from google.cloud import secretmanager
 
 client = secretmanager.SecretManagerServiceClient()
 project_id = PROJECT_ID
-
 # ただsecretをテストする
-secret_name = "just_a_test"
-resource_name = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
-response = client.access_secret_version(resource_name)
-just_a_test = response.payload.data.decode('UTF-8')
+secret_id = "just_a_test"
 
-if just_a_test == 'test1':
-    print('OK')
-else:
-    print('Check again')
+# Build the resource name of the parent project.
+parent = client.project_path(project_id)
+
+# Create the secret.
+response = client.create_secret(parent, secret_id, {
+    'replication': {
+        'automatic': {},
+    },
+})
+
+# Print the new secret name.
+print('Created secret: {}'.format(response.name))
+
+
+
+
+
+# # Build the resource name of the secret.
+# name = client.secret_path(project_id, secret_id)
+
+# # Get the current IAM policy.
+# policy = client.get_iam_policy(name)
+
+# print(policy)
+
+
+
+
+# resource_name = f"projects/{project_id}/secrets/{secret_name}/versions/1"
+# response = client.access_secret_version(resource_name)
+# just_a_test = response.payload.data.decode('UTF-8')
+
+# if just_a_test == 'test1':
+#     print('OK')
+# else:
+#     print('Check again')
