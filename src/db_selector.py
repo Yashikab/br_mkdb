14c860413adcb7ef8cc16c2f1d8d5fd4d641bd27
Yashikab <yashikaba@gmail.com>
2020-07-25 12:08:48 +0900

    add db_selector with args

--- src/db_selector.py
+++ src/db_selector.py
@@ -3,3 +3,29 @@
 '''
 google cloud sql proxyを通して, データを格納する
 '''
+import argparse
+
+
+def main():
+    msg = \
+        "MySQL DB to start.\n" \
+        "default : local \n" \
+        "if use gcs: add opt \"--gcs\""
+    parser = argparse.ArgumentParser(
+        description=msg
+    )
+    parser.add_argument(
+        '--gcs',
+        action='store_true',
+        help='if you want to use gcs as MySQL db.'
+    )
+    args = parser.parse_args()
+
+    if args.gcs:
+        print('use gcs.')
+    else:
+        print('use default')
+
+
+if __name__ == '__main__':
+    main()
