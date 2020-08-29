dd81c7e76b6f98468a0e7fb434f8740b6f59f286
Yashikab <yashikaba@gmail.com>
2020-08-29 18:54:51 +0900

    add file

---
+++ src/module/master2sql.py
@@ -0,0 +1,10 @@
+# python 3.7.5
+# coding: utf-8
+"""
+fixされたデータをsqlへ保存する。
+jyo_masterを入れる
+"""
+
+
+class JyoMaster2sql:
+    pass
---
+++ src/module/test/testdt2mysql/test_master2sql.py
@@ -0,0 +1,9 @@
+# python 3.7.5
+# coding: utf-8
+"""
+master2sqlモジュール用単体テスト
+"""
+
+
+class TestJyoMaster2sql:
+    pass
