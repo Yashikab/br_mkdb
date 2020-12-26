4524c81490199f588e36b2029f1350b2fab8682b
Yashikab <yashikaba@gmail.com>
2020-12-27 08:49:08 +0900

    create parser domain

--- src/domain/parser.py
+++ src/domain/parser.py
@@ -1,4 +1,4 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, ABCMeta, abstractmethod
 from typing import Dict, Any
 
 # TODO ここではURLの取得は行わない
@@ -9,9 +9,55 @@ from typing import Dict, Any
 class ProgramParser(meta=ABCMeta):
 
     @abstractmethod
-    def getplayerinfo2dict(self) -> Dict[str, Any]:
+    def getplayerinfo2dict(self, waku) -> Dict[str, Any]:
         pass
 
     @abstractmethod
     def getcommoninfo2dict(self) -> Dict[str, Any]:
         pass
+
+
+class ChokuzenParser(meta=ABCMeta):
+
+    @abstractmethod
+    def getplayerinfo2dict(self, waku: int) -> Dict[str, Any]:
+        pass
+
+    @abstractmethod
+    def getcommoninfo2dict(self) -> Dict[str, Any]:
+        pass
+
+
+class ResultsParser(meta=ABCMeta):
+
+    @abstractmethod
+    def getplayerinfo2dict(self, waku: int) -> Dict[str, Any]:
+        pass
+
+    @abstractmethod
+    def getcommoninfo2dict(self) -> Dict[str, Any]:
+        pass
+
+
+class OddsParser(meta=ABCMeta):
+
+    @abstractmethod
+    def three_rentan(self) -> Dict[str, float]:
+        pass
+
+    @abstractmethod
+    def three_renfuku(self) -> Dict[str, float]:
+        pass
+
+    @abstractmethod
+    def two_rentan(self) -> Dict[str, float]:
+        pass
+
+    @abstractmethod
+    def two_renfuku(self) -> Dict[str, float]:
+        pass
+
+    @abstractmethod
+    def tansho(self) -> Dict[str, float]:
+        pass
+
