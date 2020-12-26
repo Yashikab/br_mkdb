74bd652646211ef442348df6361c6f548fff81dc
Yashikab <yashikaba@gmail.com>
2020-12-26 19:11:59 +0900

    template

---
+++ src/domain/parser.py
@@ -0,0 +1,17 @@
+from abc import ABCMeta, abstractmethod
+from typing import Dict, Any
+
+# TODO ここではURLの取得は行わない
+# TODO htmlを読み込んでlxmlを使ってパースする．
+# 番組表，直前，結果，オッズを作る(抽象化)
+
+
+class ParserTemplate(meta=ABCMeta):
+
+    @abstractmethod
+    def getplayerinfo2dict(self) -> Dict[str, Any]:
+        pass
+
+    @abstractmethod
+    def getcommoninfo2dict(self) -> Dict[str, Any]:
+        pass
