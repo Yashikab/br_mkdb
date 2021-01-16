7a1aab45071a90fa19180a6dadacb1f5840aeb2a
Yashikab <yashikaba@gmail.com>
2021-01-16 11:03:41 +0900

    add TODO

--- src/infrastructure/tablecreator_impl.py
+++ src/infrastructure/tablecreator_impl.py
@@ -4,6 +4,8 @@ from domain.tablecreator import JyoDataTableCreator, JyoMasterTableCreator
 class JyoMasterTableCreatorImpl(JyoMasterTableCreator):
 
     def create_table(self):
+        # TODO sqlテキストは使わずここで作成する.
+        # TODO mysql用スキーマ組み立てがあるか調べる.
         pass
 
 
