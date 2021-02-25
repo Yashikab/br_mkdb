aaf678c1ee99de4eb2619d834838e8f1f90ac4a3
Yashikab <yashikaba@gmail.com>
2021-02-26 08:19:17 +0900

    repo

---
+++ src/domain/repository/__init__.py
@@ -0,0 +1,13 @@
+from ._chokuzeninfo import ChokuzenInfoRepository
+from ._programinfo import ProgramInfoRepository
+from ._oddsinfo import OddsInfoRepository
+from ._raceinfo import RaceInfoRepository
+from ._resultinfo import ResultInfoRepository
+
+__all__ = [
+    "RaceInfoRepository",
+    "ProgramInfoRepository",
+    "ChokuzenInfoRepository",
+    "ResultInfoRepository",
+    "OddsInfoRepository"
+]
