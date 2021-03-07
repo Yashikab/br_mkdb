a6d644fef0365db4de199d870ab16742e4093074
Yashikab <yashikaba@gmail.com>
2021-03-07 11:52:09 +0900

    test

--- src/infrastructure/test/factory/test_program.py
+++ src/infrastructure/test/factory/test_program.py
@@ -1,7 +1,9 @@
 from datetime import date
 from pathlib import Path
+from typing import Tuple
 
 import pytest
+from domain.model.info import ProgramCommonInfo, ProgramPlayerInfo
 
 from infrastructure.factory import ProgramInfoFactoryImpl
 from infrastructure.getter import GetParserContent
@@ -11,10 +13,29 @@ FILEPATH = Path(__file__).resolve().parents[1] / "test_html"
 
 class TestProgramFactoryImpl:
 
-    @pytest.mark.parametrize("target_date, target_jyo, race_no", [
-        (date(2020, 4, 8), 6, 3),
-    ])
-    def test_raceinfo(self, target_date: date, target_jyo: int, race_no: int, mocker):
+    common1 = ProgramCommonInfo(
+        "スポーツ報知　ビクトリーカップ",
+        "is_ippan",
+        "予選",
+        1800,
+        False,
+        False
+    )
+    player1_1 = (None, 0)
+    player1_2 = (None, 1)
+
+    @pytest.mark.parametrize(
+        ("target_date, target_jyo, race_no, ex_common, ex_p1, ex_p2"), [
+            (date(2020, 4, 8), 6, 3, common1, player1_1, player1_2),
+        ])
+    def test_raceinfo(self,
+                      target_date: date,
+                      target_jyo: int,
+                      race_no: int,
+                      ex_common: ProgramCommonInfo,
+                      ex_p1: Tuple[ProgramPlayerInfo, int],
+                      ex_p2: Tuple[ProgramPlayerInfo, int],
+                      mocker):
         filepath = FILEPATH / \
             f"pro_{target_date.strftime('%Y%m%d')}{target_jyo}{race_no}.html"
         lx_content = GetParserContent.file_to_content(
@@ -25,4 +46,8 @@ class TestProgramFactoryImpl:
             return_value=lx_content
         )
         pif = ProgramInfoFactoryImpl()
-        pif._raceinfo(target_date, target_jyo, race_no)
+        programinfo = pif._raceinfo(target_date, target_jyo, race_no)
+
+        assert programinfo.common == ex_common
+        assert programinfo.player[ex_p1[1]] == ex_p1[0]
+        assert programinfo.player[ex_p2[1]] == ex_p2[0]
